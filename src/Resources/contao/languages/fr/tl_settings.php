a785f2d6c8e13dc6e2acd76cb05712aef3acb167
Glen Langer <contao.modul@glen-langer.de>
2019-02-25 23:09:00 +0100

    Fixed #16, Update language files (fr)

--- src/Resources/contao/languages/fr/tl_settings.php
+++ src/Resources/contao/languages/fr/tl_settings.php
@@ -4,16 +4,31 @@
  * or to help to maintain an existing one, please register at transifex.com.
  *
  * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/contao-dlstats/language/fr/
+ * @link https://www.transifex.com/projects/p/contao-dlstats-bundle/language/fr/
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-01T22:43:29+02:00
+ * last-updated: 2017-06-04T21:13:31+02:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_settings']['dlstatdets']['0'] = 'Activer les statistiques détaillées';
-$GLOBALS['TL_LANG']['tl_settings']['dlstatdets']['1'] = 'Cocher pour journaliser chaque téléchargement selon l\'identifiant de connexion.';
-$GLOBALS['TL_LANG']['tl_settings']['dlstats']['0']    = 'Activer les statistiques des téléchargements';
-$GLOBALS['TL_LANG']['tl_settings']['dlstats']['1']    = 'Cocher pour activer le compteur de téléchargements.';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatAnonymizeIP4']['0']        = 'Niveau d\'anonymisation pour IPv4';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatAnonymizeIP4']['1']        = 'Ici, vous pouvez modifier le niveau d\'anonymisation pour IPv4. Anonymiser les adresses IP dans Contao doit être activé.';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatAnonymizeIP6']['0']        = 'Niveau d\'anonymisation pour IPv6';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatAnonymizeIP6']['1']        = 'Ici, vous pouvez modifier le niveau d\'anonymisation pour IPv6. Anonymiser les adresses IP dans Contao doit être activé.';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatDisableBotdetection']['0'] = 'Désactiver le filtrage de la détection de bot';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatDisableBotdetection']['1'] = 'Désactiver le filtrage du comptage par l\'extension Bot Detection';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatLastDownloads']['0']       = 'Nombre d\'éléments pour les derniers téléchargements';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatLastDownloads']['1']       = 'Dans la statistique, le nombre d\'éléments pour les derniers téléchargements';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatTopDownloads']['0']        = 'Nombre d\'éléments pour les téléchargements TOP';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatTopDownloads']['1']        = 'Dans la statistique, le nombre d\'éléments pour les téléchargements TOP';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatdets']['0']                = 'Activer les statistiques détaillées';
+$GLOBALS['TL_LANG']['tl_settings']['dlstatdets']['1']                = 'Cocher pour journaliser chaque téléchargement selon l\'identifiant de connexion.';
+$GLOBALS['TL_LANG']['tl_settings']['dlstats']['0']                   = 'Activer les statistiques des téléchargements';
+$GLOBALS['TL_LANG']['tl_settings']['dlstats']['1']                   = 'Cocher pour activer le compteur de téléchargements.';
+$GLOBALS['TL_LANG']['tl_settings']['dlstats']['anonip4']['1']        = 'Octets: 1 (valeur par défaut)';
+$GLOBALS['TL_LANG']['tl_settings']['dlstats']['anonip4']['2']        = 'Octets: 2';
+$GLOBALS['TL_LANG']['tl_settings']['dlstats']['anonip6']['2']        = 'Groupes: 2 (valeur par défaut)';
+$GLOBALS['TL_LANG']['tl_settings']['dlstats']['anonip6']['3']        = 'Groupes: 3';
+$GLOBALS['TL_LANG']['tl_settings']['dlstats']['anonip6']['4']        = 'Groupes: 4';
+$GLOBALS['TL_LANG']['tl_settings']['dlstats_legend']                 = 'Télécharger les statistiques';
 
