3ca80e0d0a3e3b071e2e9466164d06c74550545b
Glen Langer <contao.modul@glen-langer.de>
2023-12-17 01:33:06 +0100

    #38, logMonolog implementiert

--- src/EventListener/DownloadResponseListener.php
+++ src/EventListener/DownloadResponseListener.php
@@ -19,13 +19,13 @@ namespace BugBuster\DlstatsBundle\EventListener;
 
 use BugBuster\DLStats\DlstatsHelper;
 use Contao\CoreBundle\Framework\ContaoFramework;
-// use Contao\CoreBundle\Monolog\ContaoContext;
 use Contao\FrontendUser;
 use Contao\PageModel;
-// use Contao\System;
+use Contao\StringUtil;
 use Doctrine\DBAL\Connection;
-// use Psr\Log\LogLevel;
-use Symfony\Bundle\SecurityBundle\Security; // deprecated ab 6.2 ist Symfony\Component\Security\Core\Security;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Symfony\Bundle\SecurityBundle\Security; // Symfony\Component\Security\Core\Security ist deprecated ab 6.2
 use Symfony\Component\Filesystem\Path;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpKernel\Event\Response;
@@ -60,12 +60,6 @@ class DownloadResponseListener extends DlstatsHelper
         $this->framework->initialize();
 
         parent::__construct(); // DlstatsHelper check methods
-        // System::getContainer()
-        //     ->get('monolog.logger.contao')
-        //     ->log(LogLevel::INFO,
-        //         'DownloadResponseListener Info: construct',
-        //         ['contao' => new ContaoContext('DownloadResponseListener construct ', ContaoContext::GENERAL)])
-        //   ;
     }
 
     public function __invoke(ResponseEvent $event): void
@@ -73,25 +67,18 @@ class DownloadResponseListener extends DlstatsHelper
         if (!$event->isMainRequest() || !($response = $event->getResponse()) instanceof BinaryFileResponse) {
             return;
         }
+        $arrUniqid = StringUtil::trimsplit('.', uniqid('c0n7a0', true));
+
         /** @var PageModel $pageModel */
         $pageModel = $event->getRequest()->attributes->get('pageModel');
         if (!$pageModel instanceof PageModel) {
-            // e.g. /favicon.ico
-            // System::getContainer()
-            //     ->get('monolog.logger.contao')
-            //     ->log(LogLevel::INFO,
-            //     'DownloadResponseListener invoke: kein PageModel',
-            //     ['contao' => new ContaoContext('DownloadResponseListener MainRequest ', ContaoContext::GENERAL)])
-            // ;
+            $this->logMonolog($arrUniqid[1], __METHOD__, __LINE__, 'DownloadResponseListener invoke: no PageModel');
+
             return;
         }
         $pageHost = $event->getRequest()->getHost();
-        // System::getContainer()
-        //     ->get('monolog.logger.contao')
-        //     ->log(LogLevel::INFO,
-        //         'DownloadResponseListener invoke: ' . $pageHost,
-        //         ['contao' => new ContaoContext('DownloadResponseListener MainRequest ', ContaoContext::GENERAL)])
-        // ;
+
+        $this->logMonolog($arrUniqid[1], __METHOD__, __LINE__, 'DownloadResponseListener invoke: '.$pageHost);
 
         /** @var Response $response */
         $file = $response->getFile();
@@ -107,31 +94,66 @@ class DownloadResponseListener extends DlstatsHelper
         // Solche Responses kann es ja auch für Dateien außerhalb von files/ geben.
         // contao.upload_path injecten wie kernel.project_dir. Für die Prüfung dann Path::isBasePath nutzen
 
-        if (
-            isset($GLOBALS['TL_CONFIG']['dlstats'])
-           && true === (bool) $GLOBALS['TL_CONFIG']['dlstats']
-        ) {
+        $force = (bool) ($GLOBALS['TL_CONFIG']['dlstatsforce'] ?? false); // undocumented feature
+        $active = (bool) ($GLOBALS['TL_CONFIG']['dlstats'] ?? false);
+
+        if ($active) {
             if (
-                true === $this->DL_LOG
+                (true === $this->DL_LOG || true === $force)
                 && false === $this->checkMultipleDownload($this->_filename)
             ) {
-                $this->logDLStats();
-                $this->logDLStatDetails($pageModel, $pageHost);
+                $this->logDLStats($arrUniqid[1]);
+                $this->logDLStatDetails($pageModel, $pageHost, $arrUniqid[1]);
             }
         }
     }
 
     /**
+     * Logging via Monolog, only in debug mode.
+     *
+     * @param string $uuid
+     * @param string $class
+     * @param int    $line
+     * @param string $message
+     */
+    public function logMonolog($uuid, $class, $line, $message): void
+    {
+        if (!$this->kernel->isDebug()) {
+            return;
+        }
+
+        $timezone = new \DateTimeZone('UTC');
+        $datetime = new \DateTime('now', $timezone);
+        $strLog = 'dev-'.$datetime->format('Y-m-d').'.log';
+
+        $strLogsDir = null;
+
+        $strLogsDir = $this->kernel->getLogDir();
+
+        if (!$strLogsDir) {
+            $strLogsDir = $this->projectDir.'/var/logs';
+        }
+
+        $strMessage = sprintf("%s %s\n", $uuid, $message);
+
+        $logger = new Logger('dlstats');
+        $logger->setTimezone($timezone);
+        $logger->pushHandler(new StreamHandler($strLogsDir.'/'.$strLog, Logger::DEBUG));
+        if (false !== $class) {
+            $logger->debug($strMessage, ['class' => $class.'::'.$line]);
+        } else {
+            $logger->debug($strMessage);
+        }
+        $logger = null;
+        unset($logger);
+    }
+
+    /**
      * Helper function log file name.
      */
-    protected function logDLStats(): void
+    protected function logDLStats($Uniqid): void
     {
-        //     System::getContainer()
-        //     ->get('monolog.logger.contao')
-        //     ->log(LogLevel::INFO,
-        //         'DownloadResponseListener logDLStats',
-        //         ['contao' => new ContaoContext('DownloadResponseListener logDLStats ', ContaoContext::GENERAL)])
-        //   ;
+        $this->logMonolog($Uniqid, __METHOD__, __LINE__, 'DownloadResponseListener logDLStats');
         $q = $this->connection->fetchAssociative('SELECT id FROM `tl_dlstats` WHERE `filename`=?', [$this->_filename]);
         if (false !== $q) {
             $this->_statId = $q['id'];
@@ -154,21 +176,14 @@ class DownloadResponseListener extends DlstatsHelper
     /**
      * Helper function log details.
      */
-    protected function logDLStatDetails(PageModel $pageModel, $pageHost): void
+    protected function logDLStatDetails(PageModel $pageModel, $pageHost, $Uniqid): void
     {
-        //     System::getContainer()
-        //     ->get('monolog.logger.contao')
-        //     ->log(LogLevel::INFO,
-        //         'DownloadResponseListener logDLStatDetails',
-        //         ['contao' => new ContaoContext('DownloadResponseListener logDLStatDetails ', ContaoContext::GENERAL)])
-        //   ;
+        $this->logMonolog($Uniqid, __METHOD__, __LINE__, 'DownloadResponseListener logDLStatDetails');
         // Host / Page ID ermitteln
         $pageId = $pageModel->id;
+        $details = (bool) ($GLOBALS['TL_CONFIG']['dlstatdets'] ?? false);
 
-        if (
-            isset($GLOBALS['TL_CONFIG']['dlstatdets'])
-               && true === (bool) $GLOBALS['TL_CONFIG']['dlstatdets']
-        ) {
+        if ($details) {
             // Maximum details for year & month statistic
             $username = '';
 
